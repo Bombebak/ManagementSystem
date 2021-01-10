fa82f03339d1924fe651fc7a87947e2c34e41a51
Jakob Bak <jba@bytelab.dk>
2021-01-10 20:09:41 +0100

    Initiated application structure

---
+++ ManagementSystem/ManagementSystem.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ManagementSystem.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
