4f56ab479d44cc93442bf0ed7d0dce98a60ad7c3
Jakob Bak <jba@bytelab.dk>
2021-01-31 12:18:22 +0100

    Added description to sprint

--- ManagementSystem/ManagementSystem.Api/Data/Entities/ApplicationSprint.cs
+++ ManagementSystem/ManagementSystem.Api/Data/Entities/ApplicationSprint.cs
@@ -9,6 +9,7 @@ namespace ManagementSystem.Api.Data.Entities
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
---
+++ ManagementSystem/ManagementSystem.Api/Data/Migrations/20210131094949_TaskParentRelation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ManagementSystem.Api.Data.Migrations
+{
+    public partial class TaskParentRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Sprint",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Sprint");
+        }
+    }
+}
